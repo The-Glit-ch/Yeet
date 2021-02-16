2afea39d55e7a9c9c599feb116504f11fdd86e14
The-Glit-ch <58119800+The-Glit-ch@users.noreply.github.com>
2021-02-16 12:14:13 -0500

    Add files via upload

---
+++ Yeet.cpp
@@ -0,0 +1,31 @@
+/*
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
+*/
+#include <iostream>
+
+#define yeet using
+#define Yeet namespace
+#define YEet std
+#define YEEt ;
+#define YEET int
+#define yeeT main()
+#define yeET {
+#define yEET }
+#define YeEt cout
+#define yEeT <<
+#define yyeeeett "Yeet!\n"
+
+yeet Yeet YEet YEEt
+
+YEET yeeT
+yeET
+    YeEt yEeT yyeeeett YEEt
+yEET
+
